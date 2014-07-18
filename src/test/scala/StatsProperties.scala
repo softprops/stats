0c47d518901388ed899490e65f0bdd3bae6e2d98
softprops <d.tangren@gmail.com>
2014-07-18 00:29:56 -0400

    properties not specs

--- src/test/scala/StatsProperties.scala
+++ src/test/scala/StatsProperties.scala
@@ -57,5 +57,34 @@ object StatsSpec extends Properties("Stats") with Cleanup {
     }, 1.second)
   }
 
-  
+
+  property("time#adds") = forAll(Gen.posNum[Int]) { (i: Int) =>
+    val key = "test"
+    Await.result(for {
+      sent <- stats.time(key).add(i.milliseconds)
+      str  <- reciepts()
+    } yield {
+      sent && str == s"$key:$i|ms"
+    }, 1.second)
+  }
+
+  property("multi#sends") = forAll(for {
+    a <- Gen.posNum[Int]
+    b <- Gen.posNum[Int]
+    c <- Gen.posNum[Int]
+    d <- Gen.posNum[Int]
+  } yield (a,b,c,d)) {
+    case (a: Int, b: Int, c: Int, d: Int) =>
+      Await.result(for {
+        sent <- stats.multi(
+          stats.counter("a")(a),
+          stats.set[Int]("b").apply(b),
+          stats.gauge[Int]("c").apply(c),
+          stats.time("d")(d.milliseconds)
+        )
+        str  <- reciepts()
+      } yield {
+        sent && str == s"a:$a|c\nb:$b|s\nc:$c|g\nd:$d|ms"
+      }, 1.second)
+  }
 }
