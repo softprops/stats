52e20e708d5f4382e9e532027383a1c455a293dc
softprops <d.tangren@gmail.com>
2014-08-19 00:16:38 -0400

    address compiler warning

--- src/test/scala/Cleanup.scala
+++ src/test/scala/Cleanup.scala
@@ -1,17 +1,19 @@
 package stats
 
+import java.util.concurrent.ConcurrentHashMap
+import scala.collection.JavaConverters._
+
 trait Cleanup {
-  Cleanup.dirties += this
+  Cleanup.dirties.put(this, ())
   def cleanup()
 }
 
 object Cleanup {
-  import scala.collection.mutable.{HashSet,SynchronizedSet}
-  private val dirties = new HashSet[Cleanup] with SynchronizedSet[Cleanup]
+  private val dirties = new ConcurrentHashMap[Cleanup, Unit]
 
   def cleanup() {
     try {
-      dirties.foreach { _.cleanup() }
+      dirties.keySet.asScala.foreach { _.cleanup() }
     } catch {
       case e: Exception =>
         println("Error on cleanup")
